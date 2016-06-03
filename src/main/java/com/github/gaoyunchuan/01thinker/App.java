3ecaa1a7cf0522f24b60a6b1e5d0a27ef21b68a1
云川 高 <gaoyunchuan@gmail.com>
2016-06-03 08:58:43 +0800

    init

---
+++ src/main/java/com/github/gaoyunchuan/01thinker/App.java
@@ -0,0 +1,13 @@
+package com.github.gaoyunchuan.01thinker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
